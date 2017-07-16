3b3d9c1c414677f240ef2adab847db8e367e45b1
tmu <attwad@users.noreply.github.com>
2017-07-16 16:27:36 +0900

    fix lints in transcribe

--- transcribe/transcribe.go
+++ transcribe/transcribe.go
@@ -15,17 +15,20 @@ import (
 	longrunningpb "google.golang.org/genproto/googleapis/longrunning"
 )
 
+// ConvertToFLAC converts the input audio file into a FLAC audio file as the output filename using the program sox.
 func ConvertToFLAC(ctx context.Context, soxPath, input, output string) error {
 	ctx, cancel := context.WithTimeout(ctx, 120*time.Second)
 	defer cancel()
 	return exec.CommandContext(ctx, soxPath, input, output, "channels", "1", "rate", "16k").Run()
 }
 
+// Transcription contains what was said with a given confidence score for the overall transcription.
 type Transcription struct {
 	Text       string
 	confidence float32
 }
 
+// Transcriber allows transcription of an audio file.
 type Transcriber interface {
 	Transcribe(gcsPath string) ([]Transcription, error)
 }
@@ -35,6 +38,7 @@ type gSpeechTranscriber struct {
 	ctx    context.Context
 }
 
+// NewGSpeechTranscriber creates a new transcriber using the Google Speech API.
 func NewGSpeechTranscriber() (Transcriber, error) {
 	ctx := context.Background()
 	client, err := speech.NewClient(ctx)
