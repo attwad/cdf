4bcdf177b103dbcf21b78d09db27c7690ed672b0
tmu <attwad@users.noreply.github.com>
2017-08-24 14:00:50 +0900

    added gitignore for test files and fixed travis

--- upload/uploader_test.go
+++ upload/uploader_test.go
@@ -1,14 +1,10 @@
 package upload
 
-import (
-	"context"
-	"testing"
-)
+import "testing"
 
 func TestPath(t *testing.T) {
-	u, err := NewGCSFileUploader(context.Background(), "bucket-name-1313")
-	if err != nil {
-		t.Fatalf("creating storage client: %v", err)
+	u := &gcsFileUploader{
+		bucket: "bucket-name-1313",
 	}
 	if got, want := u.Path("file.ext"), "gs://bucket-name-1313/file.ext"; got != want {
 		t.Errorf("got=%q, want=%q", got, want)
